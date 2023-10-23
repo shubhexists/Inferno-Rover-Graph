7e8c7275e6ce5d96df8a872aa69950fc9716c905
Shubham Singh <shubh622005@gmail.com>
2023-10-23 14:21:18 +0530

    Done with random Values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ReferenceLine,
+  Cell
+} from "recharts";
 
-function App() {
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+export default function App() {
+  const [data, setData] = useState([
+    { x: 28.632430, y: 77.218790 },
+    { x: 28.632430, y: 77.218790 },
+  ]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newData = [...data];
+      newData[1].x += Math.random() < 0.5 ? -0.00001 : 0.00001;
+      newData[1].y += Math.random() < 0.5 ? -0.00001 : 0.00001;
+      setData(newData);
+      console.log(newData);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScatterChart
+      width={1900}
+      height={940}
+      margin={{
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 100
+      }}
+    >
+      <XAxis type="number" dataKey="x" name="Lattitude" domain={[data[0].x - 0.0001, data[0].x + 0.0001]} />
+      <YAxis type="number" dataKey="y" name="Longitude" domain={[data[0].y - 0.0001,data[0].y + 0.0001]}/>
+      <Tooltip cursor={{ strokeDasharray: "2 2" }} />
+      <Scatter name="A school" data={data} fill="#8884d8">
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Scatter>
+      <Tooltip cursor={{ strokeDasharray: "2 2" }} />
+      <ReferenceLine y={77.218790} stroke="#000000" />
+      <ReferenceLine x={28.632430} stroke="#000000" />
+    </ScatterChart>
   );
 }
-
-export default App;
